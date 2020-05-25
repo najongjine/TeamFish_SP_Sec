f9b8f42dc31ab81ab3cec3f02504b9b909fcfab0
najongjine <najongjin3@hotmail.com>
2020-05-25 11:58:21 +0900

    .

--- src/main/java/com/biz/tour/controller/ChatController.java
+++ src/main/java/com/biz/tour/controller/ChatController.java
@@ -1,12 +1,13 @@
 package com.biz.tour.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.biz.tour.domain.member.MemberVO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,8 +20,12 @@ public class ChatController {
 	
 	@Secured(value = {"ROLE_ADMIN","ROLE_USER"})
 	@RequestMapping(value = "",method=RequestMethod.GET)
-	public String chat(HttpSession httpSession,Model model) {
-		String u_name=(String) httpSession.getAttribute("U_NAME");
+	public String chat(Model model) {
+		MemberVO memberVO=(MemberVO) SecurityContextHolder
+				.getContext()
+				.getAuthentication()
+				.getPrincipal();
+		String u_name=memberVO.getUsername();
 		model.addAttribute("U_NAME", u_name);
 		return "chat/chat";
 	}
