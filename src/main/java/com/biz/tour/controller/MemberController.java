3d755bc3c73dfaf88c8b67151ece296d9a299e84
najongjine <najongjin3@hotmail.com>
2020-06-01 15:34:42 +0900

    .

--- src/main/java/com/biz/tour/controller/MemberController.java
+++ src/main/java/com/biz/tour/controller/MemberController.java
@@ -2,7 +2,6 @@ package com.biz.tour.controller;
 
 import javax.validation.Valid;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
